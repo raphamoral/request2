2ec1b51c82134818df50a26202f18c9f20acc149
Raphael Moral Piazera <raphamoraleng@gmail.com>
2021-10-04 02:08:47 -0300

    Lições de implementações de pytest, no final , para não remover todas as lições, apliquei  com @pytest.mark.parametrize o que reduz o for nas funções de testes.

---
+++ request2/spam/enviador_de_email.py
@@ -0,0 +1,11 @@
+class Enviador:
+    def enviar(self,remetente,destinatario,assunto,corpo):
+        if '@' not in remetente:
+            raise EmailInvalido(f'Email do remetente invalido:{remetente}')
+        return remetente
+
+
+
+class EmailInvalido(Exception):
+
+    pass
---
+++ request2/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,24 @@
+import pytest
+
+from request2.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize('destinatario', ['raphamoraleng@gmail.com', 'raphamoral@hotmail.com'])
+def test_remetente(destinatario):
+    enviador = Enviador()
+
+    resultado = enviador.enviar(destinatario, 'rmoral1991@gmail.com', 'Curse', 'Treinoo focado')
+
+    assert destinatario in resultado
+
+@pytest.mark.parametrize('remetente',['ale_moral.com','raphamoraleng'])
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(remetente, 'rmoral1991@gmail.com', 'Curse', 'Treinoo focado')
+
