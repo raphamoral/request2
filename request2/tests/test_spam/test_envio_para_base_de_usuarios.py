d288d40999ddc1b381a4c319ae0eba41496fc013
Raphael Moral Piazera <raphamoraleng@gmail.com>
2021-10-11 21:22:39 -0300

    mock de testes

--- request2/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ request2/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from request2.spam.enviador_de_email import Enviador
@@ -5,23 +7,49 @@ from request2.spam.main import EnviadorDeSpam
 from request2.spam.modelos import Usuario
 
 
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
 @pytest.mark.parametrize('usuarios',
                          [
                              [
-                               Usuario(nome='Raphael', email='raphamoraleng@gmail.com'),
-                               Usuario(nome='Renzo', email='raphamoraleng@gmail.com')
+                                 Usuario(nome='Raphael', email='raphamoraleng@gmail.com'),
+                                 Usuario(nome='Renzo', email='renzo@gmail.com')
                              ],
                              [
-                               Usuario(nome='Raphael', email='raphamoraleng@gmail.com')
+                                 Usuario(nome='Raphael', email='raphamoraleng@gmail.com')
                              ]
                          ]
                          )
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('raphamoraleng@gmail.com',
                                    'Aprendendo Junto com a Python Pro',
                                    'Confira se está acertando')
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Raphael', email='raphamoraleng@gmail.com')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails('rmoral1991@gmail.com',
+                                   'Aprendendo Junto com a Python Pro',
+                                   'Confira se está acertando')
+    enviador.enviar.assert_called_once_with == ('rmoral1991@gmail.com',
+                                            'raphamoraleng@gmail.com',
+                                            'Aprendendo Junto com a Python Pro',
+                                            'Confira se está acertando')
---
+++ request2/tests/test_spam/test_github_api.py
@@ -0,0 +1,33 @@
+from unittest.mock import Mock
+
+import pytest
+
+
+from request2 import github_api
+
+
+
+
+@pytest.fixture
+def avatar_url():
+    resp_mock = Mock()
+    url = "https://avatars.githubusercontent.com/u/80533929?v=4"
+    resp_mock.json.return_value = {"login": "raphamoral",
+                                   "id": 80533929,
+                                   "avatar_url": url
+                                   }
+    get_original = github_api.requests.get
+    github_api.requests.get = Mock(return_value=resp_mock)
+
+    yield url
+    github_api.requests.get = get_original
+
+def test_buscar_avatar(avatar_url):
+
+    url = github_api.buscar_avatar('raphamoral')
+    assert avatar_url == url
+
+def test_buscar_avatar_integracao():
+   url = github_api.buscar_avatar('raphamoral')
+   assert 'https://avatars.githubusercontent.com/u/80533929?v=4' == url
+
