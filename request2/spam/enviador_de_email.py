21f88908b1d83939992f3af69f06754a5e8f69ca
Raphael Moral Piazera <raphamoraleng@gmail.com>
2021-10-17 19:25:55 -0300

    linha 28 de teste de envio para base de usuarios flake8

--- request2/spam/enviador_de_email.py
+++ request2/spam/enviador_de_email.py
@@ -1,6 +1,5 @@
 class Enviador:
 
-
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email do remetente invalido:{remetente}')
--- request2/tests/test_spam/test_github_api.py
+++ request2/tests/test_spam/test_github_api.py
@@ -6,8 +6,6 @@ import pytest
 from request2 import github_api
 
 
-
-
 @pytest.fixture
 def avatar_url(mocker):
     resp_mock = Mock()
@@ -17,8 +15,7 @@ def avatar_url(mocker):
                                    "avatar_url": url
                                    }
     get_mock = mocker.patch('request2.github_api.requests.get')
-    get_mock.return_value=resp_mock
-
+    get_mock.return_value = resp_mock
     return url
 
 
@@ -27,7 +24,7 @@ def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('raphamoral')
     assert avatar_url == url
 
-def test_buscar_avatar_integracao():
-   url = github_api.buscar_avatar('raphamoral')
-   assert 'https://avatars.githubusercontent.com/u/80533929?v=4' == url
 
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('raphamoral')
+    assert 'https://avatars.githubusercontent.com/u/80533929?v=4' == url
